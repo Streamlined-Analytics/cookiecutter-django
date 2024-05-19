27a11c2f9d5bebefe43d01071638c300581580c0
BenA-SA <44056135+BenA-SA@users.noreply.github.com>
2024-05-19 21:15:55 +0100

    Mailgun URL Update - test commit

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -246,7 +246,7 @@ EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 ANYMAIL = {
     "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
     "MAILGUN_SENDER_DOMAIN": env("MAILGUN_DOMAIN"),
-    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.mailgun.net/v3"),
+    "MAILGUN_API_URL": env("MAILGUN_API_URL", default="https://api.eu.mailgun.net/v3"),
 }
 {%- elif cookiecutter.mail_service == 'Amazon SES' %}
 # https://anymail.readthedocs.io/en/stable/esps/amazon_ses/
