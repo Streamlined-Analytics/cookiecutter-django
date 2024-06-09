d795a2c0e53ba7466a804052577750655e728c08
ben_1 <ben@140a@hotmail.co.uk>
2024-06-09 17:58:52 +0100

    Improve forms

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/forms.py
@@ -1,5 +1,8 @@
 """
-This file is an example of how to create use htmx to add rows to the formset dynamically.
+This file has two things.
+1) A formview that uses htmx to submit the form and close the modal.
+
+2) Is an example of how to create use htmx to add rows to the formset dynamically.
 We create the base form, then create the formset, then use the formsethelper
 to style the formset (e.g. making it all a row, with a button to add more rows).
 We then use the create_new_form function to create a new formset with the new formset number.
@@ -15,13 +18,48 @@ from render_block import render_block_to_string
 from django.utils.safestring import mark_safe
 from django.shortcuts import render
 
+from django.views.generic.edit import FormView
+from django.http import HttpResponse
+from django_htmx.http import retarget
+from render_block import render_block_to_string
 
 
-class BaseForm(forms.Form):
+
+# 1) FormView that uses htmx to submit the form and close the modal.
+class BaseFormView(FormView):
+    template_name = "falcon/form-modal.html"
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponse("<script>closeModal();</script>")
+
+    def form_invalid(self, form):
+        html = render_block_to_string(
+            self.template_name,
+            "form",
+            {
+                "form": form,
+                "htmx_mode": True
+            },
+        )
+        resp = HttpResponse(html)
+        return retarget(resp, "#form-container")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["htmx_mode"] = True
+        context["normal"] = True
+        context['action_url'] = self.request.path
+        return context
+
+
+# 2) Example of how to use htmx to add rows to a formset dynamically.
+
+class BaseFormSetForm(forms.Form):
     pass
 
 
-BaseFormSet = formset_factory(BaseForm, extra=1)
+BaseFormSet = formset_factory(BaseFormSetForm, extra=1)
 
 
 class BaseFormSetHelper(FormHelper):
