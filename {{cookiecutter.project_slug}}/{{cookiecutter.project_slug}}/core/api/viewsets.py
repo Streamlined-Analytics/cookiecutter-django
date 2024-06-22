21046c0f37d6107be34d876f2609669e4d498202
ben_1 <ben@140a@hotmail.co.uk>
2024-06-22 15:59:51 +0100

    Add noqas

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/api/viewsets.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/api/viewsets.py
@@ -2,16 +2,16 @@ from rest_framework import permissions
 from rest_framework_datatables_editor.filters import DatatablesFilterBackend
 from rest_framework_datatables_editor.viewsets import DatatablesEditorModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-# from .filters import BaseFilter
-# from .serializers import BaseSerializer
+# from .filters import BaseFilter  # noqa: ERA001
+# from .serializers import BaseSerializer  # noqa: ERA001
 
 class BaseViewSet(DatatablesEditorModelViewSet):
-    # queryset = Model.objects.all()
-    # serializer_class = BaseSerializer
+    # queryset = Model.objects.all()  # noqa: ERA001
+    # serializer_class = BaseSerializer  # noqa: ERA001
     permission_classes = [permissions.IsAuthenticated]
-    # need DjangoFilterBackend for custom filters and
-    # DatatablesFilterBackend for datatables global search filtering
+    # need DjangoFilterBackend for custom filters and  # noqa: ERA001
+    # DatatablesFilterBackend for datatables global search filtering  # noqa: ERA001
     filter_backends = [DjangoFilterBackend, DatatablesFilterBackend]
-    # filterset_class = BaseFilter
+    # filterset_class = BaseFilter  # noqa: ERA001
 
 
